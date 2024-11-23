b613556676822b8633e0879fc917063fb7d31ba0
Harsher <gillharsher1@gmail.com>
2024-11-23 12:29:40 +0530

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,37 +3,37 @@ import Link from "next/link";
 import React from "react";
 
 const navIcons = [
-  {src: '/assets/icons/search.svg', alt: 'search'},
-  {src: '/assets/icons/black-heart.svg', alt: 'black-heart'},
-  {src: '/assets/icons/user.svg', alt: 'user'},
-]
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "black-heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+];
 
 const Navbar = () => {
   return (
     <header className="w-full">
       <nav className="nav">
         <Link href="/" className="flex items-center gap-1">
-        <Image 
-          src="/assets/icons/logo.svg"
-          height={27}
-          width={27}
-          alt="logo"
-        />
-        <p className="nav-logo">
-          Price<span className="text-primary">Wise</span>
-        </p>
+          <Image
+            src="/assets/icons/logo.svg"
+            height={27}
+            width={27}
+            alt="logo"
+          />
+          <p className="nav-logo">
+            Price<span className="text-primary">Wise</span>
+          </p>
         </Link>
         <div className="flex items-end gap-5">
-            {navIcons.map((icon) => (
-              <Image 
-                key={icon.alt}
-                src={icon.src}
-                alt={icon.alt}
-                height={27}
-                width={27}
-                className="object-contain"
-              />
-            ))}
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              height={27}
+              width={27}
+              className="object-contain"
+            />
+          ))}
         </div>
       </nav>
     </header>
